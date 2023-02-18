32e6dc16be4fb2a1d374af2a351b5e371bd1a929
Priyanshu Kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2023-02-18 15:25:16 +0530

    Added table prgram in java

---
+++ table.java
@@ -0,0 +1,9 @@
+class table{
+    public static void main (String args[]){
+        int num = 10;
+        System.out.println("The table of "+num+" is");
+        for(int i = 1;i<=10;i++){
+            System.out.println(num+" x "+i+" = "+(num*i));
+        }
+    }
+}
