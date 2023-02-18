03d1f8f03dce9ae884224744ce236433041cfd72
Priyanshu Kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2023-02-18 15:32:23 +0530

    Program of sum from 1 to 10 added

---
+++ SUM1.java
@@ -0,0 +1,9 @@
+class SUM1{
+    public static void main(String args[]){
+        int sum = 0;
+        for(int i=1;i<=10;i++){
+            sum = sum + i;
+        }
+        System.out.print("The sum of numbers between 1 to 10 is "+ sum);
+    }
+}
