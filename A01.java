065e44b46ebb49a16e257e16595d04d6f46dc061
Priyanshu Kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2023-02-21 20:21:52 +0530

    Sum of first 10 integers.

---
+++ A01.java
@@ -0,0 +1,11 @@
+// To find the sum of first ten integers.
+
+class A01{
+    public static void main(String args[]){
+        int sum = 0;
+        for(int i=1;i<=10;i++){
+            sum = sum + i;
+        }
+        System.out.print("The sum of numbers between 1 to 10 is "+ sum);
+    }
+}
