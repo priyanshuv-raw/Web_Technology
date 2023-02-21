a2132fbb739c2f2ef281ce28f6aa178ad792e8b2
Priyanshu Kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2023-02-21 20:32:24 +0530

    Finding Sum & Diff using parent and child class.

--- A05.java
+++ A05.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
-class calcmain{
+class A05{
     public static void main(String args[]){
         Scanner Sc = new Scanner(System.in);
         int x,y,z;
         calcs obj = new calcs();
         calcd ob = new calcd();
+        System.out.print("Enter the first number:");
         x = Sc.nextInt();
+        System.out.print("Enter the second number:");
         y = Sc.nextInt();
         obj.sum(x,y);
         ob.diff(x,y);
