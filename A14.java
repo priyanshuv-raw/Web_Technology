dffa784d8e7e7f1e537752f4abf702cf1cf5f671
Priyanshu Kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2023-02-22 20:53:25 +0530

    Program to convert datatype from one to another.

---
+++ A14.java
@@ -0,0 +1,12 @@
+// Program to convert datatype from one to another.
+
+class A14{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        a = Integer.parseInt(args[0]);
+        b = Integer.parseInt(args[1]);
+        c = a+b;
+        System.out.println("Sum:"+c);
+    }
+}
